1956936b9e41cca59a39942c7bed75c30b4e34ef
chakalaUltra <ivanxx7777@gmail.com>
2025-10-15 00:08:20 +0300

    Update securityhh.py

--- securityhh.py
+++ securityhh.py
@@ -18,7 +18,7 @@ import math
 CLIENT_ID = os.environ.get("CLIENT_ID")
 CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-REDIRECT_URI = "https://1627243d-109a-4d82-9166-8dd61f9129c0-00-s1i93gsr0j8e.worf.replit.dev/oauth/callback"
+REDIRECT_URI = "https://sg-verification.onrender.com/oauth/callback"
 
 CONFIG_PATH = "server_configs.json"
 BLACKLISTED_PATH = "blacklisted_servers.json"
